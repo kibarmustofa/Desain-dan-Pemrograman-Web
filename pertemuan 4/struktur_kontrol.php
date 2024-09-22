f06c05038f64f511e488f887472e85f67a68826b
kibarmustofa <kibarmustofa115@gmail.com>
2024-09-22 15:00:47 +0700

    percobaan 4 soal (4.3)

--- pertemuan 4/struktur_kontrol.php
+++ pertemuan 4/struktur_kontrol.php
@@ -10,4 +10,33 @@ if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
 } else if ($nilaiNumerik >= 60 && $nilaiNumerik < 70) {
     echo "Nilai huruf: D ";
 }
+
+echo "<br><br>";
+
+$jarakSaatIni = 0;
+$jarakTarget = 500;
+$peningkatanHarian = 30;
+$hari = 0;
+
+while ($jarakSaatIni < $jarakTarget) {
+    $jarakSaatIni += $peningkatanHarian;
+    $hari++;
+}
+
+echo "Atlet terebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
+
+echo "<br><br>";
+
+$jumlahLahan = 10;
+$tanamanPerlahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i = 1; $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($tanamanPerlahan * $buahPerTanaman);
+}
+
+echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+
 ?>
+
