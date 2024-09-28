1079c294b8385855e552e54d6d3961fa93f4c514
kibarmustofa <kibarmustofa115@gmail.com>
2024-09-28 21:35:49 +0700

    percobaan 2 fungsi rekursif

--- pertemuan 5/PHP/rekursif.php
+++ pertemuan 5/PHP/rekursif.php
@@ -1,45 +1,9 @@
 <?php
-$menu = [
-    [
-        "nama" => "Beranda"
-    ],
-    [
-        "nama" => "Berita",
-        "subMenu" => [
-            [
-                "nama" => "Wisata",
-                "subMenu" => [
-                    [
-                        "nama" => "Pantai"
-                    ],
-                    [
-                        "nama" => "Gunung"
-                    ]
-                ]
-            ],
-            [
-                "nama" => "Kuliner"
-            ],
-            [
-                "nama" => "Hiburan"
-            ]
-        ]
-    ],
-    [
-        "nama" => "Tentang"
-    ],
-    [
-        "nama" => "Kontak"
-    ],
-];
+function TampilkanHaloDunia(){
+    echo"Halo dunia!  <br>";
 
-function tampilkanMenuBertingkat(array $menu) {
-    echo "<ul>";
-    foreach ($menu as $key => $item) {
-        echo "<li>{$item['nama']}</li>";
-    }
-    echo "</ul>";
+    TampilkanHaloDunia();
 }
 
-tampilkanMenuBertingkat($menu);
+TampilkanHaloDunia();
 ?>
