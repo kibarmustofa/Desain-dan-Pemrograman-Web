59a6af77047db4f75f70874dda18894d67604108
kibarmustofa <kibarmustofa115@gmail.com>
2024-09-28 20:38:24 +0700

    percobaan 1 parameter dengan nilai default

--- pertemuan 5/PHP/fungsi.php
+++ pertemuan 5/PHP/fungsi.php
@@ -1,6 +1,6 @@
 <?php
 // membuat fungsi
-function perkenalan($nama, $salam){
+function perkenalan($nama, $salam = "Assalamualaikum"){
     echo $salam . ". ";
     echo "Perkenalkan, nama saya " . $nama . "<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
@@ -14,6 +14,6 @@ echo "<hr>";
 $saya = "Kibar";
 $ucapanSalam = "Selamat pagi";
 
-// memanggil lagi
-perkenalan($saya, $ucapanSalam);
+// memanggil lagi taboa mengisi parameter salam
+perkenalan($saya);
 ?>
