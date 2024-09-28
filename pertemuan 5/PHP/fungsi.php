63d60320c63c32338247898c3362fbb846d22022
kibarmustofa <kibarmustofa115@gmail.com>
2024-09-28 20:18:37 +0700

    percobaan1 fungsi php bagian 1

---
+++ pertemuan 5/PHP/fungsi.php
@@ -0,0 +1,9 @@
+<?php
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Kibar mustofa<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+//memanggil fungsi yang sudah dibuat 
+perkenalan();
+?>
