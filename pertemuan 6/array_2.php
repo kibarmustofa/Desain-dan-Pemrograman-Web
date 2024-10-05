9b192da4e4e7445e8425471a472c298281f983c0
kibarmustofa <kibarmustofa115@gmail.com>
2024-10-05 16:00:11 +0700

    percobaan 2 modifikasi menjadi tabel

--- pertemuan 6/array_2.php
+++ pertemuan 6/array_2.php
@@ -1,22 +1,58 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title></title>
-</head>   
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Profil Dosen</title>
+    <style>
+      table {
+            width: 50%;
+            margin: 20px auto;
+            border-collapse: collapse;
+            font-family: Arial, sans-serif;
+        }
+        th, td {
+            padding: 12px;
+            border: 1px solid lightgray; 
+            text-align: left;
+        }
+        th {
+            background-color: green; 
+            color: white;
+        }
+        tr:nth-child(even) {
+            background-color: lightgray; 
+        }
+    </style>
+</head>
 <body>
-  <?php
-    $Dosen = [
-      "nama" => "Elok Nur Hamdana",
-      "domisili" => "Malang",
-      "jenis_kelamin" => "Perempuan"
-    ];
 
-    echo "Nama : " . $Dosen["nama"] . "<br>";
-    echo "Domisili : " . $Dosen["domisili"] . "<br>";
-    echo "Jenis Kelamin : " . $Dosen["jenis_kelamin"] . "<br>";
-  ?>
+<?php
+    $Dosen = [
+        "nama" => "Elok Nur Hamdana",
+        "domisili" => "Malang",
+        "jenis_kelamin" => "Perempuan"
+    ];
+?>
+
+<table>
+    <tr>
+        <th>Label</th>
+        <th>Data</th>
+    </tr>
+    <tr>
+        <td>Nama</td>
+        <td><?php echo $Dosen["nama"]; ?></td>
+    </tr>
+    <tr>
+        <td>Domisili</td>
+        <td><?php echo $Dosen["domisili"]; ?></td>
+    </tr>
+    <tr>
+        <td>Jenis Kelamin</td>
+        <td><?php echo $Dosen["jenis_kelamin"]; ?></td>
+    </tr>
+</table>
+
 </body>
 </html>
