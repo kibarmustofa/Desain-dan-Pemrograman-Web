b893bc80977f4a359914886c6b43fa2e36015620
kibarmustofa <kibarmustofa115@gmail.com>
2024-10-12 19:14:50 +0700

    percobaan 2 empty percobaan part1-2

---
+++ pertemuan 7/empty.php
@@ -0,0 +1,19 @@
+<?php
+echo"empty percobaan 1 <br>";
+$myArray = array(); 
+if (empty($myArray)) {
+    echo " Array tidak teridentifikasi atau kosong.<br>";
+}else{
+    echo "Array terdefinisi dan tidak kosong.<br>";
+}
+echo "<br>";
+echo"empty percobaan 2 <br>";
+if (empty($myVariable)) {
+    echo "Variabel belum diinisialisasi.<br>";
+}
+else{
+    echo "Array terdefinisi dan tidak kosong.<br>";
+}
+
+
+?>
