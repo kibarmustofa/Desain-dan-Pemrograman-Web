2a891e20005d409f7b226f72ab07e89d59e5b20a
kibarmustofa <kibarmustofa115@gmail.com>
2024-10-13 07:45:00 +0700

    percobaan 5 penggunaan regex  bagian 5 modifikasi (?)

--- pertemuan 7/regex.php
+++ pertemuan 7/regex.php
@@ -29,7 +29,7 @@ $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text; // Output: "I like banana pie."
 
 echo "<br><br>";
-echo "memastikan kata yang hampir cocok dari segi patern dari text sample<br>";
+echo "memastikan kata yang hampir cocok dari segi o	*: 0 atau lebih kali  patern dari text sample<br>";
 
 $pattern = '/go*d/'; // Cocokkan "god", "good", "gooood", dll.
 $text = 'god is good.';
@@ -39,4 +39,16 @@ if (preg_match($pattern, $text, $matches)) {
 } else {
     echo "Tidak ada yang cocok!";
 }
+echo "<br><br>";
+echo "memastikan kata yang hampir cocok dari segi ?: 0 atau 1 kali  patern dari text sample<br>";
+
+$pattern = '/go?d/'; // Cocokkan "gd", "god", "good", dll.
+$text = 'god is good.';
+
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
+?>
 ?>
