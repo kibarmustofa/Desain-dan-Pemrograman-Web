837567120b86bc4f66657cc85dda073218a29769
kibarmustofa <kibarmustofa115@gmail.com>
2024-10-20 06:55:48 +0700

    percobaan 1 upload file modifikasi ukuran maksimal

--- pertemuan 8/upload.php
+++ pertemuan 8/upload.php
@@ -4,15 +4,15 @@ if (isset($_POST["submit"])) {
     $target_file = $target_dir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    $maxsize = 5 * 1024 * 1024;
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $maxsize = 3 * 1024 * 1024;
 
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)) {
             echo "File berhasil diunggah.<br>";
 
-            // Menampilkan gambar thumbnail dengan lebar 200px
-            echo '<img src="' . $target_file . '" width="200" style="height: auto;">';
+            //  Menampilkan gambar thumbnail dengan lebar 200px
+            // echo '<img src="' . $target_file . '" width="200" style="height: auto;">';
         } else {
             echo "Gagal mengunggah file.";
         }
