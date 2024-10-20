0449448913d937bb6b2f34ce0e4f46d21ca1b440
kibarmustofa <kibarmustofa115@gmail.com>
2024-10-20 15:15:06 +0700

    percobaan 3 upload file ajax modifikasi multi file ekstensi gambar pada upload dan form

--- pertemuan 8/upload_ajax.php
+++ pertemuan 8/upload_ajax.php
@@ -1,25 +1,36 @@
 <?php
 if (isset($_FILES['file'])) {
     $errors = array();
-    $file_name = $_FILES['file']['name'];
-    $file_size = $_FILES['file']['size'];
-    $file_tmp = $_FILES['file']['tmp_name'];
-    @$file_type = strtolower(end(explode('.', $_FILES['file']['name']))."");
-    $extensions = array("pdf", "doc", "docx", "txt");
+    $extensions = array("jpg", "jpeg", "png", "gif"); // Ekstensi gambar yang diizinkan
+    $maxSize = 2097152; 
 
-    if (in_array($file_ext, $extensions) === false) {
-        $errors[] = "Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, atau TXT.";
-    }
+    // Menghitung jumlah file yang diunggah
+    $totalFiles = count($_FILES['file']['name']);
 
-    if ($file_size > 2097152) {
-        $errors[] = "Ukuran file tidak boleh lebih dari 2 MB";
-    }
+    for ($i = 0; $i < $totalFiles; $i++) {
+        $file_name = $_FILES['file']['name'][$i];
+        $file_size = $_FILES['file']['size'][$i];
+        $file_tmp = $_FILES['file']['tmp_name'][$i];
+        $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION)); // Menggunakan pathinfo untuk mendapatkan ekstensi file
 
-    if (empty($errors) == true) {
-        move_uploaded_file($file_tmp, "documents/".$file_name);
-        echo "File berhasil diunggah.";
-    } else {
-        echo implode("", $errors);
+        
+        if (!in_array($file_ext, $extensions)) {
+            $errors[] = "$file_name: Ekstensi file yang diizinkan hanya JPG, JPEG, PNG, dan GIF.";
+        }
+
+        
+        if ($file_size > $maxSize) {
+            $errors[] = "$file_name: Ukuran file tidak boleh lebih dari 2 MB.";
+        }
+
+      
+        if (empty($errors)) {
+            move_uploaded_file($file_tmp, "documents/" . $file_name);
+            echo "$file_name berhasil diunggah.<br>";
+        } else {
+            echo implode("<br>", $errors) . "<br>";
+            $errors = array(); 
+        }
     }
 }
 ?>
