6678865bee81c083f90a81ed4d198d0ecc28009c
kibarmustofa <kibarmustofa115@gmail.com>
2024-10-20 07:27:02 +0700

    percobaan 2 multi uplad modifikasi upload khusus gambar

--- pertemuan 8/form_multiupload.php
+++ pertemuan 8/form_multiupload.php
@@ -6,7 +6,9 @@
 <body>
     <h2>Unggah Dokumen</h2>
     <form action="proses_multiupload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="files[]" multiple="multiple" accept=".pdf, .doc, .docx">
+    <!--digunakan untuk uplaod file ekstensi dokumen-->
+    <!-- <input type="file" name="files[]" multiple="multiple" accept=".pdf, .doc, .docx"> -->
+    <input type="file" name="files[]" multiple="multiple" accept=".jpeg, .jpg, .gif, .png">
         <input type="submit" value="Unggah">
     </form>
 </body>
